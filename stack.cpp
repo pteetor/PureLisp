566b5a03cbc3f5e11bcc19935406e2a1bd38b42a
Paul Teetor <paulteetor@yahoo.com>
2025-10-31 23:33:13 -0500

    Change stack from upward-growing to downward-growing

--- stack.cpp
+++ stack.cpp
@@ -1,29 +1,34 @@
 //
 //  Stack functions
 //
+// NOTE: This stacks grows *downward*, not upward.
+//
 
 #include "consVM.h"
 
 const int STACK_SIZE = 1000;
 Cell* stack[STACK_SIZE];
 Cell** sp;
-Cell** stack_limit;
+Cell** stack_base;
 
 void init_stack()
 {
-  sp = &stack[0] - 1;
-  stack_limit = stack + STACK_SIZE;
+  stack_base = stack + STACK_SIZE;
+  sp = stack_base;
 }
 
 Cell* top()
 {
+  if (sp == stack_base) {
+    fatal("top: stack underflow");
+  }
   return *sp;
 }
 
 void push(Cell* p)
 {
-  ++sp;
-  if (sp == stack_limit) {
+  --sp;
+  if (sp == stack) {
     fatal("push: stack overflow");
   }
   *sp = p;
@@ -31,7 +36,9 @@ void push(Cell* p)
 
 Cell* pop()
 {
-  // TODO: Check for underflow
-  return *sp--;
+  if (sp == stack_base) {
+    fatal("pop: stack underflow");
+  }
+  return *sp++;
 }
 

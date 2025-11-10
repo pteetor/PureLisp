8fbde41996616b2a5b38c7ed12fd2e46271c7a03
Paul Teetor <paulteetor@yahoo.com>
2025-11-10 09:32:33 -0600

    Add test

--- test-harness.cpp
+++ test-harness.cpp
@@ -5,3 +5,9 @@
 TEST_CASE("000 - initialization") {
   init_consvm();
 }
+
+// For the heap's sake, Free, Atom, and Cons must all have same size
+TEST_CASE("Heap correctly laid out") {
+  REQUIRE(sizeof(Free) == sizeof(Cons));
+  REQUIRE(sizeof(Free) == sizeof(Atom));
+}
